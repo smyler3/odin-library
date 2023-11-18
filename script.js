d84ce569af892c0e459f2a710a1039f59e78f6fe
smyler3 <mylertalym01@gmail.com>
2023-11-18 15:04:02 +1100

    Finish modal logic and new book creation logic
    The modal now correctly opens and closes, performs it's own checks on
    entered data, clears on closing, and adds new books to the library

--- script.js
+++ script.js
@@ -1,13 +1,75 @@
+const MIN_PAGE_NUMBER = 1;
+
 /* Creates event listeners for interacting with the new book modal */
 function createModalEventListeners() {
     const addBookModal = document.querySelector("#new-book-modal");
     const showBookModalBtn = document.querySelector("#show-book-modal");
     const closeBookModalBtn = document.querySelector("#close-book-modal");
-    const submitBookBtn = document.querySelector("#submit-new-book");
+    const newBookForm = document.querySelector("#new-book-form");
 
+    // Display modal
     showBookModalBtn.addEventListener("click", () => {
         addBookModal.showModal();
     });
+
+    // Clear form and close modal
+    closeBookModalBtn.addEventListener("click", () => {
+        resetBookForm();
+        addBookModal.close();
+    })
+
+    // Check form on submission, clear it, and close modal
+    newBookForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let title = document.getElementById("book-title");
+        let author = document.getElementById("book-author");
+        let pages = document.getElementById("book-pages");
+        let read = document.getElementById("book-read");
+        console.log(title.value, author.value, pages.value, read.value);
+
+        // Book doesn't meet criteria
+        if (title.value === "") {
+            title.classList.add("error-field");
+        }
+        else if (author.value === "") {
+            author.classList.add("error-field");
+        } 
+        else if (pages.value < MIN_PAGE_NUMBER) {
+            pages.classList.add("error-field");
+        }    
+        // Book meets criteria
+        else {
+            title.classList.remove("error-field");
+            author.classList.remove("error-field");
+            pages.classList.remove("error-field");
+
+            addBookToLibrary(new Book(title.value, author.value, pages.value, read.value));
+            displayBooks();
+            resetBookForm();
+            addBookModal.close();
+        }
+    })
+}
+
+function resetBookForm() {
+    const newBookForm = document.querySelector("#new-book-form");
+    let title = document.getElementById("book-title");
+    let author = document.getElementById("book-author");
+    let pages = document.getElementById("book-pages");
+
+    // Removing error indicators
+    title.classList.remove("error-field");
+    author.classList.remove("error-field");
+    pages.classList.remove("error-field");
+    // Clearing form
+    newBookForm.reset();
+}
+
+// Sets the minimum page number for books in the library
+function setMinPages() {
+    const pagesField = document.getElementById("book-pages");
+
+    pagesField.setAttribute("min", MIN_PAGE_NUMBER);
 }
 
 /* Object representing a book */
@@ -16,6 +78,7 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.rendered = false;
 }
 
 /* Returns a string explaining the book information */
@@ -35,8 +98,16 @@ function displayBooks() {
 
     // Creates a book card for each book
     for (let book of myLibrary) {
-        let bookCard = createBookCard(book); 
-        books.appendChild(bookCard);
+        // Book already on screen
+        if (book.rendered) {
+            continue;
+        }
+        // Book yet to be displayed
+        else {
+            let bookCard = createBookCard(book); 
+            books.appendChild(bookCard);
+            book.rendered = true;
+        }
     }
 }
 
@@ -69,18 +140,15 @@ function createBookCard(book) {
 
 // Setup Functions
 createModalEventListeners();
+setMinPages();
 
 // Initial Data
 const myLibrary = [];
 
-const theRingSetsOut = new Book("The Ring Sets Out", "J.R.R Tolkien", 1168, true);
-const theRingGoesSouth = new Book("The Ring Goes South", "J.R.R Tolkien", 253, false);
-const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, false);
-
 // Create Library Collection
-addBookToLibrary(theRingSetsOut);
-addBookToLibrary(theRingGoesSouth);
-addBookToLibrary(theHobbit);
+addBookToLibrary(new Book("The Ring Sets Out", "J.R.R Tolkien", 1168, true));
+addBookToLibrary(new Book("The Ring Goes South", "J.R.R Tolkien", 253, false));
+addBookToLibrary(new Book("The Hobbit", "J.R.R Tolkien", 295, false));
 
 // Display on page
 displayBooks();
